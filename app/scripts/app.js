e0d5bed75c3ac054e071991e95599d14d341c99f
plaidpizazz <marcellus.spears@gmail.com>
2017-03-25 11:39:12 -0500

    Checkpoint 3 statues: ui-sref update completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     function config($stateProvider, $locationProvider) {
         $locationProvider
             .html5Mode({
@@ -14,6 +14,10 @@
             .state('album', {
                 url: '/album',
                 templateUrl: '/templates/album.html'
+            })
+            .state('collection', {
+                url: '/collection',
+                templateUrl: '/templates/collection.html'
             });
     }
     
