6bf04c14214b6bd211544f2f9de4310f03012641
plaidpizazz <marcellus.spears@gmail.com>
2017-04-11 00:20:00 -0500

    Assignment 7 services2: completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -25,12 +25,24 @@
             
             currentSong = song;
         };
-        
+        /**
+        * @function playSong
+        * @desc Starts playing the currently selected song and sets the song.playing Boolean flag
+        * @param {Object} song
+        */
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        };
+        /**
+        * @method SongPlayer.play
+        * @desc Checks to see if the song clicked was already the selected song.  If not, then sets the clicked song to the current song and plays it. If is the selected song and is paused then resumes playing.
+        * @param {Object} song
+        */
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
                 setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song);
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
                     currentBuzzObject.play();
@@ -38,7 +50,11 @@
             }
             
         };
-        
+        /**
+        * @method SongPlayer.pause
+        * @desc Pauses the currently playing song and clears the song.playing Boolean flag
+        * @param {Object} song
+        */
         SongPlayer.pause = function(song) {
             currentBuzzObject.pause();
             song.playing = false;
